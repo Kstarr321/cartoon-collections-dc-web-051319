3a43b3dec47d373c3cf61fd600522066d9376b18
Kolton Starr <starrkolton@gmail.com>
2019-05-01 14:33:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ def long_planeteer_calls(arr)
   end 
 end 
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arr)
+  arr.
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
